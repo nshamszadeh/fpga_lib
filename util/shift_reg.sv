6b0666feca2eb05430f65cbee9dafeae3a0a04e3
Navid <nshamszadeh@gmail.com>
2026-09-21 22:56:34 -0700

    Yes.

--- util/shift_reg.sv
+++ util/shift_reg.sv
@@ -1,9 +1,8 @@
-`define LEFT  0
-`define RIGHT 1
+import util_pkg::shift_dir_t;
 
 module shift_reg_in #(
-    parameter int DATA_WIDTH = 8,
-    parameter int DIR        = `LEFT
+    parameter int         DATA_WIDTH = 8,
+    parameter shift_dir_t DIR        = LEFT
 )(
     input logic clk,
     input logic rst_n,
@@ -13,27 +12,31 @@ module shift_reg_in #(
 );
     generate
         case (DIR)
-            `LEFT: begin
+            LEFT: begin
                 always_ff @(posedge clk) begin
-                    if (rst_n) out_reg <= '0;
-                    else begin
-                        if (en) begin
-                            // shift left
-                        end
-                    end
+                    if (!rst_n) out_reg <= '0;
+                    else if (en) out_reg <= {out_reg[DATA_WIDTH-2:0], data};
                 end
             end
-            `RIGHT: begin
+            RIGHT: begin
                 always_ff @(posedge clk) begin
                     if (rst_n) out_reg <= '0;
-                    else begin
-                        if (en) begin
-                            // shift right
-                        end
-                    end
+                    else if (en) out_reg <= {data, out_reg[DATA_WIDTH-1:1]};
                 end
             end
-
         endcase
     endgenerate
+endmodule
+
+module shift_reg_out #(
+    parameter int DATA_WIDTH  = 8,
+    parameter shift_dir_t DIR = LEFT
+)(
+    input  logic                  clk,
+    input  logic                  rst_n,
+    input  logic                  en,
+    input  logic [DATA_WIDTH-1:0] data_reg,
+    output logic                  out_data
+);
+
 endmodule
---
+++ util/util_pkg.sv
@@ -0,0 +1,3 @@
+package util_pkg;
+    typedef enum logic { LEFT = 1'b0, RIGHT = 1'b1} shift_dir_t;
+endpackage
